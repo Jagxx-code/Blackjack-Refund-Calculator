9d8be541e5cf4536a5ab8de1b7ec7860abcf13b6
James <gonzalesjaames@gmail.com>
2025-08-01 14:50:27 +0800

    7th Commit

--- script.js
+++ script.js
@@ -17,30 +17,14 @@ function calculateRefund() {
       alert("Commission Calculator coming soon!");
     }
 
-    // Replace this with your actual Google Apps Script Web App URL
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxC8uWUwt4lIZwEGBKGkPahfbiR6utHpGdwNRGrRJxH3IsRW4ZkVb8jRDa9FCJoojpqww/exec';
-
-const form = document.getElementById('refundForm');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const data = {
-    price: form.price.value,
-    totalDays: form.totalDays.value,
-    remainingDays: form.remainingDays.value,
-    serviceFee: form.serviceFee.value,
-  };
-
-  fetch(scriptURL, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json' },
-  })
-  .then(res => res.json())
-  .then(response => {
-    alert('Submitted to Google Sheet!');
-    form.reset();
-  })
-  .catch(error => alert('Error submitting: ' + error.message));
-});
+fetch('https://script.google.com/macros/s/AKfycbxC8uWUwt4lIZwEGBKGkPahfbiR6utHpGdwNRGrRJxH3IsRW4ZkVb8jRDa9FCJoojpqww/exec', {
+  method: 'POST',
+  body: JSON.stringify(data),
+  mode: 'no-cors',
+  headers: { 'Content-Type': 'application/json' },
+})
+.then(() => {
+  alert('Data submitted!');
+  form.reset();
+})
+.catch(err => alert('Submission failed: ' + err.message));
