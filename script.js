9bd89f60278b12d46c800a37c98975ceee61aba0
James <gonzalesjaames@gmail.com>
2025-08-01 14:39:54 +0800

    Sixth Commit

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ function calculateRefund() {
     function calculateCommission() {
       alert("Commission Calculator coming soon!");
     }
+
+    // Replace this with your actual Google Apps Script Web App URL
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxC8uWUwt4lIZwEGBKGkPahfbiR6utHpGdwNRGrRJxH3IsRW4ZkVb8jRDa9FCJoojpqww/exec';
+
+const form = document.getElementById('refundForm');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const data = {
+    price: form.price.value,
+    totalDays: form.totalDays.value,
+    remainingDays: form.remainingDays.value,
+    serviceFee: form.serviceFee.value,
+  };
+
+  fetch(scriptURL, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' },
+  })
+  .then(res => res.json())
+  .then(response => {
+    alert('Submitted to Google Sheet!');
+    form.reset();
+  })
+  .catch(error => alert('Error submitting: ' + error.message));
+});
